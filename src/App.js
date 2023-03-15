5e096be0ac27e6ed29a80ac18b2d836d805dc127
Dane Albuquerque <dayalb_pas@hotmail.com>
2023-03-15 18:35:51 -0300

    feat: criação de titulo variavel

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard title="Meu Painel de Controle 2.0" />
+    </div>
+  );
+}
+
+export default App;
